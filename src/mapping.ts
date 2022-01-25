6543dfb04056a615e53264e841864d2bdfff7330
Rishub-Handa <rh4gh@virginia.edu>
2022-01-25 09:13:43 -0500

    wrote subgraph to track AuctionBid AuctionCreated and AuctionSettled as independent entities

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,55 +1,62 @@
 import { BigInt } from "@graphprotocol/graph-ts"
 import {
   AuctionHouse,
-  AdminChanged,
-  BeaconUpgraded,
-  Upgraded
+
+  AuctionBid,
+  AuctionCreated,
+  AuctionSettled,
+
 } from "../generated/AuctionHouse/AuctionHouse"
-import { ExampleEntity } from "../generated/schema"
+import { AuctionBidEntity, AuctionCreatedEntity, AuctionSettledEntity } from "../generated/schema"
 
-export function handleAdminChanged(event: AdminChanged): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
+export function handleAuctionBid(event: AuctionBid): void {
 
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (!entity) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
+  // const { wizardId, aId, sender, value, extended } = event.params;
 
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
-  }
+  const id = `${event.params.wizardId.toHex()}:${event.params.sender.toHex()}:${event.params.value.toHex()}`
+  let entity = new AuctionBidEntity(id);
 
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
+  entity.wizardId = event.params.wizardId;
+  entity.aId = event.params.aId;
+  entity.sender = event.params.sender;
+  entity.value = event.params.value;
+  entity.extended = event.params.extended;
 
-  // Entity fields can be set based on event parameters
-  entity.previousAdmin = event.params.previousAdmin
-  entity.newAdmin = event.params.newAdmin
+  entity.save();
 
-  // Entities can be written to the store with `.save()`
-  entity.save()
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.admin(...)
-  // - contract.implementation(...)
 }
 
-export function handleBeaconUpgraded(event: BeaconUpgraded): void {}
+export function handleAuctionCreated(event: AuctionCreated): void {
 
-export function handleUpgraded(event: Upgraded): void {}
+
+  // const { wizardId, aId, startTime, endTime, oneOfOne, isWhitelistDay } = event.params;
+  let entity = new AuctionCreatedEntity(event.params.wizardId.toHex());
+
+  entity.wizardId = event.params.wizardId;
+  entity.aId = event.params.aId;
+  entity.startTime = event.params.startTime;
+  entity.endTime = event.params.endTime;
+  entity.oneOfOne = event.params.oneOfOne;
+  entity.isWhitelistDay = event.params.isWhitelistDay;
+
+  entity.save();
+
+}
+
+export function handleAuctionSettled(event: AuctionSettled): void {
+
+  // const { wizardId, aId, winner, amount } = event.params;
+  let entity = new AuctionSettledEntity(event.params.wizardId.toHex());
+
+  entity.wizardId = event.params.wizardId;
+  entity.aId = event.params.aId;
+  entity.winner = event.params.winner;
+  entity.amount = event.params.amount;
+
+  entity.save();
+
+}
+
+
+
+
